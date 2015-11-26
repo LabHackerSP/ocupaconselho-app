4ca0e680746736b483eff7525a0663a40a217f69
Pedro Markun <pedro@markun.com.br>
2015-11-26 10:15:15 -0200

    movendo coisas para o config

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var candidatos_sheet = 'https://docs.google.com/spreadsheets/d/1f5rpmZr09cvJDGhk4L-3ZkAvmWuutUP1iBNQ4tzt2H4/edit#gid=1239846838'
+
+var local_sheet = 'https://docs.google.com/spreadsheets/d/1kWpm-G3dAFwVQxkH6n_BNZqJUkNAkUrpZ6ZjBuff9gw/edit#gid=1275282574';
+
+var subs_sheet = 'https://docs.google.com/spreadsheets/d/1f5rpmZr09cvJDGhk4L-3ZkAvmWuutUP1iBNQ4tzt2H4/edit#gid=1601481367';
+
+var tags_sheet = 'https://docs.google.com/spreadsheets/d/1f5rpmZr09cvJDGhk4L-3ZkAvmWuutUP1iBNQ4tzt2H4/edit#gid=1200910956';
