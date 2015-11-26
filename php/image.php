9699d8fcc2ac9a98508b2f3f1130a05749f70259
Pedro Markun <pedro@markun.com.br>
2015-11-26 11:04:17 -0200

    user agent para baixar imagens

--- php/image.php
+++ php/image.php
@@ -12,7 +12,7 @@ if (file_exists($arquivo)) {
 }
 else {
 	$base_url = "http://localhost:8080/cola.html" . $argh;
-	$image = shell_exec('./wkhtmltoimage --quality 100 "'. $base_url . '" ' . $arquivo);
+	$image = shell_exec('./wkhtmltoimage --quality 100 --custom-header "User-Agent" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0" "'. $base_url . '" ' . $arquivo);
 	echo imagejpeg(imagecreatefrompng($arquivo));
 }
 ?>
