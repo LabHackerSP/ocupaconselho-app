5358bae6981f93fbb40f5858bd0aebbf35ed6bea
Pedro Markun <pedro@markun.com.br>
2015-11-27 09:47:31 -0200

    servername contextual no image.php nao funciona com php -S

--- php/image.php
+++ php/image.php
@@ -11,7 +11,7 @@ if (file_exists($arquivo)) {
 	echo imagejpeg(imagecreatefrompng($arquivo));
 }
 else {
-	$base_url = "http://localhost:8080/cola.html" . $argh;
+	$base_url = $_SERVER['SERVER_NAME'] . "/cola.html" . $argh;
 	$image = shell_exec('./wkhtmltoimage --quality 100 --custom-header "User-Agent" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0" "'. $base_url . '" ' . $arquivo);
 	echo imagejpeg(imagecreatefrompng($arquivo));
 }
